49815d71b97c4c138925eef7e4465d329c33f0e5
Ruben Ramirez Briseño <ruben.23292@gmail.com>
2022-11-14 09:44:05 -0600

    Hook - useReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useReducer, useEffect } from 'react'
+
+interface AuthState {
+  validando: boolean
+  token: string | null
+  username: string
+  nombre: string
+}
+
+const initialState: AuthState = {
+  validando: true,
+  token: null,
+  username: '',
+  nombre: ''
+}
+
+type AuthAction = { type: 'logout'}
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState  => {
+  switch(action.type) {
+    case 'logout':
+      return {
+        ...state,
+        validando: false,
+        token: null,
+        nombre: '',
+        username: ''
+      }
+    default:
+      return state
+  }
+}
 
 const Login = () => {
+  const [{ validando }, dispatch] = useReducer(authReducer, initialState)
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'logout' })
+    }, 1500)
+  }, [])
+
+  if(validando) {
+    return (
+      <>
+         <h3>Login</h3>
+         <div className='alert alert-info'>Validando...</div>
+      </>
+    )
+  }
+
   return (
     <>
       <h3>Login</h3>
-
-      <div className='alert alert-info'>Validando</div>
 
       <div className='alert alert-danger'>No autenticado</div>
 
