c32d9b0a4a27aaa425292d9950d532793d95a7d4
Ruben Ramirez Briseño <ruben.23292@gmail.com>
2022-11-18 17:34:07 -0600

    Custom hook useForm

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react';
+import { useForm } from '../hooks/useForm';
 
 const Formularios = () => {
-  const [formulario, setFormulario] = useState({
+  const { formulario, email, password, onChange } = useForm({
     email: 'text@gmail.com',
     password: '123456'
   })
-
-  const onChange = ( value: string, campo: string ) => {
-    setFormulario({
-      ...formulario,
-      [campo]: value 
-    })
-  }
 
   return (
     <>
@@ -21,14 +14,15 @@ const Formularios = () => {
         type="text" 
         className="form-control" 
         placeholder="Email" 
-        value={formulario.email}
+        value={email}
         onChange={ ({ target }) => onChange(target.value, 'email') }
       />
+
       <input
         type="text"
         className="form-control mt-2 mb-2"
         placeholder="Password"
-        value={formulario.password}
+        value={password}
         onChange={ ({ target }) => onChange(target.value, 'password') }
       />
 
