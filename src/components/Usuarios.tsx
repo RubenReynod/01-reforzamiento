296090d915f94dfcf1f5d5e24fc39af41b22acd3
Ruben Ramirez Briseño <ruben.23292@gmail.com>
2022-11-18 00:43:46 -0600

    Mostrar usuarios en pantalla

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -8,10 +8,23 @@ const Usuarios = () => {
   useEffect(() => {
     reqResApi.get<ReqResListado>('/users')
       .then( resp => {
-        console.log(resp.data.data)
+        setUsuarios(resp.data.data)
       })
       .catch( console.log )
   }, [])
+
+  const rendetItem = (usuario: Usuario) => {
+    const { id, first_name, last_name, email, avatar } = usuario
+    return (
+      <tr key={ id+'' }>
+        <td>
+          <img src={ avatar } alt={ first_name } style={{ width: 50, borderRadius: 35 }} />
+        </td>
+        <td>{ first_name } { last_name }</td>
+        <td>{ email }</td>
+      </tr>
+    )
+  }
 
   return (
     <>
@@ -26,9 +39,11 @@ const Usuarios = () => {
           </tr>
         </thead>
         <tbody>
-
+          { usuarios.map( usuario => rendetItem(usuario) ) }
         </tbody>
       </table>
+
+      <button className='btn btn-primary'>Siquientes</button>
     </>
   )
 }
