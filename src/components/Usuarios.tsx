88cde4b65b3d0cdb207a737edb2057f5264fc29e
Ruben Ramirez Briseño <ruben.23292@gmail.com>
2022-11-18 00:35:27 -0600

    Tipados de la respuesta HTTP

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { reqResApi } from '../api/reqRes';
+import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
 const Usuarios = () => {
+  const [usuarios, setUsuarios] = useState<Usuario[]>([])
+  
   useEffect(() => {
-    reqResApi.get('/users')
+    reqResApi.get<ReqResListado>('/users')
       .then( resp => {
         console.log(resp.data.data)
       })
